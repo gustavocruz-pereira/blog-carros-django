c3398457987fcc852759cc4c5969a582bb244be4
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-08-28 17:51:35 -0300

    adicionado comentarios

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Post
+from comments.models import Comentario
 
 def home(request):
     posts = Post.objects.all()
@@ -7,4 +8,5 @@ def home(request):
 
 def post(request, post_id):
     posts = Post.objects.get(pk=post_id)
-    return render(request, 'post.html', {'posts': posts})
+    comentarios = Comentario.objects.get(pk=post_id)
+    return render(request, 'post.html', {'posts': posts, 'comentarios': comentarios})
