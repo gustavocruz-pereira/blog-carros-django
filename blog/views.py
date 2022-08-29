f50cf848ace3dda13ba54d3d2f9e02bc566a9a1e
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-08-29 11:23:56 -0300

    melhorias nos comentarios

--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,6 @@ def home(request):
 
 def post(request, post_id):
     posts = Post.objects.get(pk=post_id)
-    comentarios = Comentario.objects.get(pk=post_id)
+    comentarios = Comentario.objects.filter(post_comentario=post_id)
     return render(request, 'post.html', {'posts': posts, 'comentarios': comentarios})
+    
