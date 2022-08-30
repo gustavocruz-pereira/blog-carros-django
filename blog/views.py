3f84e07727f3066ec8345caed9f52bdf19dc2ad5
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-08-29 21:34:37 -0300

    adicionado produtos

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from blog.models import Post
 from comments.models import Comentario
+from produtos.models import Produtos
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+    produtos = Produtos.objects.all()
+    return render(request, 'home.html', {'posts': posts, 'produtos': produtos})
 
 def post(request, post_id):
     posts = Post.objects.get(pk=post_id)
--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,14 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+
+
+class Produtos(models.Model):
+    nome_produto = models.CharField(max_length=150, verbose_name='Produto')
+    descr_produto = models.CharField(max_length=150, blank=True)
+    preco_produto = models.FloatField()
+    imagem_produto = models.ImageField(upload_to="media/uploads/%Y/%m/%d")
+
+
+    def __str__(self) -> str:
+        return self.nome_produto
