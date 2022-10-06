074534eadcdc9d10666d68a6615234f010e83d93
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-10-06 17:21:00 -0300

    pasta

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('artigos/', views.artigos, name='artigos'),
-    path('mercadorias/', views.mercadorias, name='mercadorias'),
-]
