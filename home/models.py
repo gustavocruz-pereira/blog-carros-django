ed50bd250e512df402dc65dc52fb66949670fcce
Gustavo Cruz <guz.cruz.07@gmail.com>
2023-03-29 21:32:07 -0300

    Vou recomeçar esse projeto do zero

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     summary = models.CharField(max_length=255)
@@ -14,4 +15,6 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
 
